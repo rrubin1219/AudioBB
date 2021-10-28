2b0bc046e5c8c827e449dfb3aeda534b1e4fba6d
Rachel <tul52326@temple.edu>
2021-10-27 23:11:29 -0400

    Added Book Object

---
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -0,0 +1,31 @@
+package edu.temple.audiobb
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Book(val title: String, val author: String): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(author)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Book> {
+        override fun createFromParcel(parcel: Parcel): Book {
+            return Book(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Book?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
