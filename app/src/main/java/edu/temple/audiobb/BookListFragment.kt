50b4b18415154e124e7e16a18150f54c322d4c41
Rachel <tul52326@temple.edu>
2021-10-27 23:31:51 -0400

    Created List Fragment

--- app/src/main/java/edu/temple/audiobb/BookListFragment.kt
+++ app/src/main/java/edu/temple/audiobb/BookListFragment.kt
@@ -5,54 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val BOOK_KEY = "books"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BookListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BookListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var list : Array<Book>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            list = it.getParcelableArray(BOOK_KEY) as Array<Book>
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false)
+        val layout = inflater.inflate(R.layout.fragment_book_list, container, false)
+
+        val recyclerView = layout.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val itemViewModel = ViewModelProvider(requireActivity()).get(ItemViewModel::class.java)
+        val onClickListener = View.OnClickListener {
+            val position = recyclerView.getChildAdapterPosition(it)
+            itemViewModel.setItem(list[position])
+        }
+
+        //recyclerView.adapter = BookAdapter(list, onClickListener)
+
+        return layout
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(list: Array<Book>) =
             BookListFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                   putParcelableArray(BOOK_KEY, list)
                 }
             }
     }
